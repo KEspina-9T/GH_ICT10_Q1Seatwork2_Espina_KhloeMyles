460f56ab34f2bf3c2dd1382d948a8905679742d6
KEspina-10T <espina.ka@obmontessori.edu.ph>
2025-09-14 19:55:55 +0800

    main.py

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 #  this pyscript is for integrating your python code to your webpage
 from pyscript import display
 
+
 r_name= 'Riverbend Brew' #restaurant name to be on display
 o_name= 'Owner: Khloe Myles S. Espina' #Owner name to be on display
 year= 'Since 2025' #Year of foundation
 prod1='Jungle Grove Latté' #1st product 
-price1='₱120' #{}
-prod2='Mossy Matcha Cheescake'
-price2='₱130'
-prod3='Flowering Hibuscus Tea'
-price3='₱90'
-prod4='Rose Petal Tart'
-price4='₱160'
-prod5='Maple Syrup Waffles'
-price5='₱140'
-schedule='Open Daily | 7:00am - 9pm'
+price1='₱120' #1st price
+prod2='Mossy Matcha Cheescake' #2nd product
+price2='₱130' # 2nd price
+prod3='Flowering Hibuscus Tea' #3rd product
+price3='₱90' #3rd price
+prod4='Rose Petal Tart' #4th product
+price4='₱160' #4th price
+prod5='Maple Syrup Waffles' #5th product
+price5='₱140' #5th price
+schedule='Open Daily | 7:00am - 9pm' #Opening hours
 
 display(f'{r_name}', target="result")
 display(f'{o_name}', target="result1")
@@ -30,5 +31,6 @@ display(f'{price3}', target="price3")
 display(f'{price4}', target="price4")
 display(f'{price5}', target="price5")
 display(f'{schedule}', target="Sched")
+
 
 
